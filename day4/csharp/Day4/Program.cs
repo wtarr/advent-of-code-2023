bd6670ce361b839643115f94ecce80b1032f4aa4
Will <william.projects.2012@gmail.com>
2023-12-04 15:31:20 +0000

    Part 2 complete
    With a little help from the internet

--- day4/csharp/Day4/Program.cs
+++ day4/csharp/Day4/Program.cs
@@ -8,30 +8,107 @@ var dir = "F:\\Documents\\GitHub\\Aoc2023\\input";
 
 var input = "day4.txt";
 
-var numbers = Utilities.ReadAllNumbers($"{dir}\\{input}");
+var allCards = Utilities.ReadAllNumbers($"{dir}\\{input}");
 
-var total = 0;
-
-foreach (var card in numbers)
+int Part1()
 {
-    var matches = Utilities.GetMatchingWinningNumbers(card);
-    var score = Utilities.Score(matches.Count);
+    var total = 0;
 
-    total += score;
+    foreach (var card in allCards)
+    {
+        var matches = Utilities.GetMatchingWinningNumbers(card);
+        var score = Utilities.Score(matches.Count());
+
+        total += score;
+    }
+
+    return total;
 }
 
-Console.WriteLine($"Part 1 : Total score: {total}");
+Console.WriteLine($"Part 1 : Total score: {Part1()}");
+
+
+// needed help with this one to wrap my head around the problem - (hyper-neutrino) https://youtu.be/uxShpk__9xE?si=a75x3BDIrERsD3VYp
+int Part2()
+{
+    var map = new Dictionary<int, int>();
+
+    foreach (var card in allCards)
+    {
+        map.TryAdd(card.Id, 1); // first encounter?
+
+        var matches = Utilities.GetMatchingWinningNumbers(card);
+
+        foreach (var winningCopyId in Enumerable.Range(card.Id + 1, matches.Count()))
+        {
+            map.TryAdd(winningCopyId, 1);
+
+            map[winningCopyId] += map[card.Id];
+        }
+    }
+    
+    return map.Values.Sum();
+}
+
+Console.WriteLine($"Part 2 : Total score: {Part2()}");
+
+
+// recursively solving it ...
+// memory + speed is not good
+int Part2Recursively()
+{
+    var tally = 0;
+
+    foreach (var card in allCards)
+    {
+        tally += 1; // current card
+        RecurseGames(card);
+    }
+
+    void RecurseGames(ScratchCard game)
+    {
+        var wins = Utilities.GetMatchingWinningNumbers(game);
+
+        var enumerable = wins as int[] ?? wins.ToArray();
+        
+        tally += enumerable.Length; // this card wins x amount of copies
+
+        // for each of the id's of the winning copies, recurse (where we have in our collection of all cards)
+        foreach (var winningCopyId in Enumerable.Range(game.Id + 1, enumerable.Length))
+        {
+            var winningCopy = Utilities.SelectById(allCards, winningCopyId).FirstOrDefault();
+
+            if (winningCopy == null)
+                return;
+
+            RecurseGames(winningCopy);
+        }
+    }
+
+    return tally;
+}
+
+Console.WriteLine($"Part 2 (recursive) : Total score: {Part2Recursively()}");
 
 public class Utilities
 {
+    public static List<ScratchCard> SelectById(List<ScratchCard> numbers, int id)
+    {
+        return numbers.Where(n => n.Id == id).ToList();
+    }
+    
+    public static List<ScratchCard> SelectByIds(List<ScratchCard> numbers, List<int> ids)
+    {
+        return numbers.Where(n => ids.Contains(n.Id)).ToList();
+    }
 
-    public static List<Numbers> ReadAllNumbers(string path)
+    public static List<ScratchCard> ReadAllNumbers(string path)
     {
         // Card   1: 95 57 30 62 11  5  9  3 72 87 | 94 72 74 98 23 57 62 14 30  3 73 49 80 96 20 60 17 35 11 63 87  9  6  5 95
         
         var lines = File.ReadAllLines(path);
         
-        var numbers = new List<Numbers>();
+        var numbers = new List<ScratchCard>();
 
         foreach (var line in lines)
         {
@@ -42,7 +119,7 @@ public class Utilities
             var winningNumbers = ReadNumbers(numbersParts[0].Trim());
             var yourNumbers = ReadNumbers(numbersParts[1].Trim());
             
-            numbers.Add(new Numbers { Id = id, WinningNumbers = winningNumbers, YourNumbers = yourNumbers });
+            numbers.Add(new ScratchCard { Id = id, WinningNumbers = winningNumbers, YourNumbers = yourNumbers });
         }
 
         return numbers;
@@ -70,10 +147,10 @@ public class Utilities
         return numbers;
     }
 
-    public static List<int> GetMatchingWinningNumbers(Numbers numbers)
+    public static IEnumerable<int> GetMatchingWinningNumbers(ScratchCard scratchCard)
     {
         // find the winning numbers that are in your numbers
-        var winningNumbers = numbers.YourNumbers.Intersect(numbers.WinningNumbers).ToList();
+        var winningNumbers = scratchCard.YourNumbers.Intersect(scratchCard.WinningNumbers);
 
         return winningNumbers;
     }
@@ -86,7 +163,7 @@ public class Utilities
     
 }
 
-public class Numbers
+public class ScratchCard
 {
     public int Id { get; set; }
     public List<int> WinningNumbers { get; set; }
