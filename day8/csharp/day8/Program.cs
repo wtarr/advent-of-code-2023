320b6fad42b48a3183be6256a9f147ac1df2ef2b
Will <william.projects.2012@gmail.com>
2023-12-08 07:54:22 +0000

    day 8 part 2

--- day8/csharp/day8/Program.cs
+++ day8/csharp/day8/Program.cs
@@ -24,60 +24,92 @@ foreach (var line in lines.Skip(2))
     // AAA = (BBB, CCC)
     var parts = line.Split(" = ");
     var name = parts[0].Trim();
-    
+
     // remove the brackets
     var left = parts[1].Trim().Substring(1, parts[1].Length - 2).Split(", ")[0];
     var right = parts[1].Trim().Substring(1, parts[1].Length - 2).Split(", ")[1];
-    
+
     if (!graph.ContainsKey(name))
     {
         graph.Add(name, new Node(name));
     }
-    
-    
+
+
     graph[name].Left = left;
     graph[name].Right = right;
-    
 }
 
 
-
-var found = false;
+// Part 1
 var start = "AAA";
 var end = "ZZZ";
 
-var current = graph[start];
+var p1Start = graph[start];
 
-var steps = 0;
+var p1Steps = StepsUntil(p1Start.Name, s => s.Equals(end), path);
 
-while (!found)
+Console.WriteLine($"Part 1: steps required {p1Steps}");
+
+// part 2
+
+var startsWithA = graph.Keys.Where(x => x.EndsWith('A')).ToList();
+var p2Steps = new List<ulong>();
+
+foreach (var n in startsWithA)       
 {
-    steps++;
-    
-    var p = path.Dequeue();
-    
-   
-    
-    if (p == 'L')
+    path.Clear();
+    foreach (var p in instructions.Trim())
     {
-        current = graph[current.Left];
-    }
-    else
-    {
-        current = graph[current.Right];
-    }
-
-    if (current.Name == end)
-    {
-        found = true;
+        path.Enqueue(p);
     }
     
-    path.Enqueue(p);
+    var steps = StepsUntil(n, s => s.EndsWith('Z'), path);
     
+    p2Steps.Add(steps);
 }
 
-Console.WriteLine($"Part 1: steps required {steps}");
+// get the lowest common multiple of p2Steps
+var lowestCommonMultiple = p2Steps.Aggregate((a, b) => (a * b) / GreatestCommonDenominator(a, b));
 
+
+ulong GreatestCommonDenominator(ulong a, ulong b)
+{
+    while (b != 0)
+    {
+        var temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    return a;
+}
+
+Console.WriteLine($"Part 2 : steps required {lowestCommonMultiple}");
+
+ulong StepsUntil(string startNode, Func<string, bool> endCondition, Queue<char> path)
+{
+    var found = false;
+
+    var current = graph[startNode];
+    ulong steps = 0;
+    while (!found)
+    {
+        steps++;
+
+        var p = path.Dequeue();
+
+        current = p == 'L' ? graph[current.Left] : graph[current.Right];
+
+        if (endCondition.Invoke(current.Name))
+        {
+            found = true;
+        }
+
+        path.Enqueue(p);
+    }
+
+    return steps;
+}
 
 public class Node
 {
@@ -87,7 +119,7 @@ public class Node
     }
 
     public string Name { get; set; }
-    
+
     public string Left { get; set; }
     public string Right { get; set; }
 }
