0980c46797341649359e577c6cf9a3b5ae5f9cd4
Will <william.projects.2012@gmail.com>
2023-12-09 13:01:28 +0000

    day 9 part 2
    just take part 1 and reverse each line of the input and repeat as if part 1

--- day9/csharp/day9/Program.cs
+++ day9/csharp/day9/Program.cs
@@ -8,61 +8,81 @@ var input = "day9.txt";
 
 var lines = File.ReadAllLines(Path.Combine(directory, input));
 
-var numberLine = lines.Select(
+var numberLine1 = lines.Select(
         line => line.Split(Array.Empty<char>()))
     .Select(split => split.Select(int.Parse).ToList())
     .ToList();
 
-var tally = 0;
-foreach (var nl in numberLine)
+var part1Tally = ReduceDeduct(numberLine1);
+
+Console.WriteLine($"Part 1 : {part1Tally}");
+
+
+var numberLine2 = lines.Select(
+        line => line.Split(Array.Empty<char>()))
+    .Select(split => split.Select(int.Parse).ToList())
+    .ToList();
+
+// reverse each line in numberLine2
+var reversed = numberLine2.Select(nl => nl.Reverse<int>().ToList()).ToList();
+
+var part2Tally = ReduceDeduct(reversed);
+
+Console.WriteLine($"Part 2 : {part2Tally}");
+
+int ReduceDeduct(List<List<int>> numberLine)
 {
-    var reductions = new List<List<int>>();
-
-    reductions.Add(nl);
+    var tally = 0;
     
-    var reduced = false;
-    while (!reduced)
+    foreach (var nl in numberLine)
     {
-        // take last from reductions
-        var lastAdded = reductions.Last();
-        
-        var current = new List<int>();
+        var reductions = new List<List<int>>();
+
+        reductions.Add(nl);
     
-        var zipped = lastAdded.Zip(lastAdded.Skip(1), (a, b) => (a, b));
-        
-        foreach (var (a, b) in zipped)
+        var reduced = false;
+        while (!reduced)
         {
-            current.Add(b - a);
-        }
+            // take last from reductions
+            var lastAdded = reductions.Last();
         
-        reductions.Add(current);
+            var current = new List<int>();
+    
+            var zipped = lastAdded.Zip(lastAdded.Skip(1), (a, b) => (a, b));
+        
+            foreach (var (a, b) in zipped)
+            {
+                current.Add(b - a);
+            }
+        
+            reductions.Add(current);
 
-        // if current is all same
-        if (current.All(i => i == current.First()))
+            // if current is all same
+            if (current.All(i => i == current.First()))
+            {
+                reduced = true;
+            }
+        
+            //reduced = current.All(i => i == 0);
+        }
+    
+        var reversed = reductions.Reverse<List<int>>().ToList();
+
+        for (var i = 1; i < reversed.Count(); i++)
         {
-            reduced = true;
+            var current = reversed.ElementAt(i).Last();
+            var previous = reversed.ElementAt(i - 1).Last();
+        
+            reversed[i].Add(current + previous);
         }
-        
-        //reduced = current.All(i => i == 0);
-    }
-    
-    var reversed = reductions.Reverse<List<int>>().ToList();
 
-    for (var i = 1; i < reversed.Count(); i++)
-    {
-        var current = reversed.ElementAt(i).Last();
-        var previous = reversed.ElementAt(i - 1).Last();
-        
-        reversed[i].Add(current + previous);
+        var last = reversed.Last().Last();
+
+        tally+= last;
+    
+        Console.WriteLine(last);
+    
     }
 
-    var last = reversed.Last().Last();
-
-    tally+= last;
-    
-    Console.WriteLine(last);
-    
+    return tally;
 }
-
-Console.WriteLine($"Part 1 : {tally}");
-
